ba0e2dc3f0e30bf652ed1be3e401f6517fd11d0e
johannes <jmuljana@gmail.com>
2021-10-06 11:21:33 +1100

    Changed app.js and add a readme

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello There!</Text>
+      <Text>Hello There! This is a test app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,2 @@
+# Test app using React Native
+This app is created to test React Native
