bc82235b6b8ee703e3ff949207feeda4b8281d69
johannes <jmuljana@gmail.com>
2021-10-06 11:55:17 +1100

    added Flatlist component

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
+import Constants from 'expo-constants'
+
+const Item = (props) => {
+  return(
+    <View>
+      <Text>{props.text}</Text>
+    </View>
+  )
+}
 
 export default function App() {
+
+  const AppData = [
+    { id: "1", name: "Apple"},
+    { id: "2", name: "Orange"},
+    { id: "3", name: "Banana"},
+    { id: "4", name: "Blueberry"},
+    { id: "5", name: "Tomato"}
+  ]
+
+  const Renderer = ({item}) => ( <Item text={item.name} /> )
+
   return (
     <View style={styles.container}>
       <Text>Hello There! This is a test app!</Text>
-      <StatusBar style="auto" />
+      <FlatList data={AppData} keyExtractor={ (item) => item.id } renderItem={Renderer} />
     </View>
   );
 }
@@ -15,7 +35,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
 });
